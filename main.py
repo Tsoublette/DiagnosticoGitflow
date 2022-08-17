832c4eb87c46bb43091343d4d540539011eab813
Tomas Soublette Ruiz de Gamboa <tomassoublette@MacBook-Air-de-Tomas.local>
2022-08-17 17:01:39 -0400

    primer avance

---
+++ main.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+print("Bienvenido")
+print("Qué funcion deseas utilizar:")
+print("1. Los top 10 tweets más retweeted.")
+print("2. Los top 10 usuarios en función de la cantidad de tweets que emitieron.")
+print("3. Los top 10 días donde hay más tweets.")
+print("4. Los top 10 hashtags más usados.")
+
+resp = input("ingresa una opción:")
+print("Opción: " + str(resp))
